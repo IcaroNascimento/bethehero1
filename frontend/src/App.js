5dd61e4353d28340c655a363f29d7e75077bf2de
IcaroNascimento <icaronascimento1@gmail.com>
2020-04-11 14:44:50 -0300

    feature: create 'Logon' frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import Header from "./Header";
+import './global.css';
 
+import Logon from './pages/Logon';
 
 function App() {
-  const [counter, setCounter] = useState(0)
-
-  function increment() {
-    setCounter(counter + 1)
-    console.log("see my increment counter:", counter)
-  }
-
   return (
-    <div>
-      <Header>Contador: {counter} </Header>
-      <button onClick={increment}>Incrementar</button>
-    </div>
+    <Logon />
   );
 }
 
